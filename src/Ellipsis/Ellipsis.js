a38776712acf2ca9540d0d0e55956ca1e8e00336
ernestchakhoyan <chakhoyanernest@gmail.com>
2019-05-19 18:33:53 +0400

    Working components

--- src/Ellipsis/Ellipsis.js
+++ src/Ellipsis/Ellipsis.js
@@ -2,25 +2,22 @@ import React from "react";
 import styles from "./Ellipsis.css";
 
 const Ellipsis = props => (
-  <>
-    <style>{styles.toString()}</style>
-    <div className="lds-css ng-scope">
-      <div className="lds-spinner" style="width:100%;height:100%">
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
+  <div className="lds-css ng-scope">
+    <div className="lds-spinner" style="width:100%;height:100%">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
-  </>
+  </div>
 );
 
 export default Ellipsis;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-export * from "./Ellipsis";
-
 export * from "./Ring";
 
 export * from "./Ripple";
